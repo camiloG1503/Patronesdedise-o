72056fb33c899eeb9bb2e22d8945120b22b5e53b
Camilo Gómez <gomezcamilo1503@gmail.com>
2025-03-21 08:15:38 -0500

    Conectando proyecto a GitHub

---
+++ src/Builder/ASCIIConverter.java
@@ -0,0 +1,20 @@
+package Builder;
+
+public class ASCIIConverter extends Converter {
+
+    public ASCIIConverter(String document) {
+        super(document);
+    }
+
+    public void mainLines(){
+        System.out.println();
+    }
+
+    public void makeParagraph(){
+        System.out.println();
+    }
+
+    public void makeTable(){
+        System.out.println();
+    }
+}
