59eb4e495f8ad497291d25875eb7f4e7f08323d3
Camilo Gómez <gomezcamilo1503@gmail.com>
2025-03-21 07:49:44 -0500

    Conectando proyecto a GitHub

---
+++ src/Builder/Converter.java
@@ -0,0 +1,14 @@
+package Builder;
+
+public class Converter {
+
+    private String document;
+
+    public Converter(String document) {
+        this.document = document;
+    }
+
+    public void mainLines(){
+        System.out.println();
+    }
+}
